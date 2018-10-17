8ff7d228898dd6942f019d3282fd5a48df3144b3
Boaz Berman <berman.boaz@gmail.com>
2018-10-17 18:25:33 +0300

    WIP: Fetching events to show transactions page

--- src/views/Transactions.vue
+++ src/views/Transactions.vue
@@ -12,8 +12,8 @@ import AppHeader from "@/components/AppHeader";
 import Balance from "@/components/Balance";
 import TokenJson from "../build/contracts/token.json";
 import { thorify } from "thorify";
-import Web3 from 'web3';
-import { debug } from 'util';
+import Web3 from "web3";
+import { debug } from "util";
 
 export default {
   name: "Transactions",
@@ -25,22 +25,26 @@ export default {
     return {};
   },
   mounted() {
-    const web3 = thorify(new Web3(), "http://localhost:8669");
-    web3.eth.getBlock("latest").then(num=>{
-    console.log(this.$Contract)
-    console.log(num);
-    this.$Contract.getPastEvents('Transfer', {fromBlock:0, to:num.number-1},(error, eventResult) => {
-         console.log("Error in myEvent event handler: " + error);
-         console.log("myEvent: " + JSON.stringify(eventResult));
-      })
-     // .on("data", event => console.log("EthPurchase Event", event));
+    const web3 = thorify(new Web3(), "https://vethor-node.digonchain.com/");
+    web3.eth.getBlock("latest").then(num => {
+      console.log(this.$Contract);
+      console.log(num);
+      this.$Contract.getPastEvents(
+        "Transfer",
+        { limit: 10, from: 0 },
+        (error, eventResult) => {
+          debugger;
+          console.log("Error in myEvent event handler: " + error);
+          console.log("myEvent: " + JSON.stringify(eventResult));
+        }
+      );
+      // .on("data", event => console.log("EthPurchase Event", event));
 
-   
-    
-    const address = "0x0000000000000000000000000000456e65726779";
-    const Token = new web3.eth.Contract(TokenJson, address);
-  })}}
-
+      const address = "0x0000000000000000000000000000456e65726779";
+      const Token = new web3.eth.Contract(TokenJson, address);
+    });
+  }
+};
 </script>
 
 <style scoped>
